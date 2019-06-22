5606745bb9ebf75f13c5a134981c92773da13d12
ajuthomas <ajuthomas@christophersair.lan>
2019-06-22 19:20:30 -0400

    junit basic files

---
+++ src/test/java/com/thomas/junit/MyMathTest.java
@@ -0,0 +1,15 @@
+package com.thomas.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTest {
+
+	@Test
+	public void test() {
+		
+		//fail("Not yet implemented"); 
+	}
+
+}
